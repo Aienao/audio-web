f595f8c9d34e70c8c459a7eef41beaa1cb28faa4
Aienao <dev@ainoe.com>
2022-07-30 10:06:19 +0800

    init

--- src/components/AudioUpload.vue
+++ src/components/AudioUpload.vue
@@ -8,36 +8,61 @@
         :on-change="fileListChange"
         drag
         action="none"
+        :show-file-list="false"
     >
-      <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+      <el-icon class="el-icon--upload">
+        <upload-filled/>
+      </el-icon>
       <div class="el-upload__text">
         拖拽文件到此区域 或者 <em>点我上传</em>
       </div>
     </el-upload>
     <el-select v-model="audioFormat" placeholder="格式（默认MP3）">
-      <el-option label="mp3" value="mp3" />
-      <el-option label="flac" value="flac" />
+      <el-option label="mp3" value="mp3"/>
+      <el-option label="flac" value="flac"/>
     </el-select>
     <el-select v-model="audioBitRate" placeholder="比特率" v-if="audioFormat === 'mp3'">
-      <el-option label="320Kbs" value="320Kbs" />
-      <el-option label="256Kbs" value="256Kbs" />
-      <el-option label="128Kbs" value="128Kbs" />
+      <el-option label="320Kbs" value="320Kbs"/>
+      <el-option label="256Kbs" value="256Kbs"/>
+      <el-option label="128Kbs" value="128Kbs"/>
     </el-select>
-    <el-button class="ml-3" type="success" @click="uploadFileList()">
+    <el-button type="primary" @click="uploadFileList()">
       开始转换
     </el-button>
+  </div>
+  <div>
+    <el-table
+        :data="fileList"
+        style="width: 100%"
+        v-if="fileList.length > 0"
+        @selection-change="getSelections"
+    >
+      <el-table-column type="selection" width="55"/>
+      <el-table-column property="name" label="名称"/>
+      <el-table-column property="size" label="大小" :formatter="formatAudioSize"/>
+      <el-table-column label="操作">
+        <template #default="scope">
+          <el-button type="danger" circle @click="deleteFile(scope.row.name)">
+            <el-icon style="vertical-align: middle">
+              <Delete/>
+            </el-icon>
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 import api from './../api/api';
+
 export default {
   name: "AudioUpload",
   data() {
     return {
       fileList: [],
-      audioFormat:'',
-      audioBitRate:'',
+      audioFormat: '',
+      audioBitRate: '',
     }
   },
   methods: {
@@ -46,12 +71,19 @@ export default {
     },
     uploadFileList() {
       let formData = new FormData();
-      formData.append("format",this.audioFormat);
-      formData.append("birRate",this.audioBitRate);
+      formData.append("format", this.audioFormat);
+      formData.append("birRate", this.audioBitRate);
       this.fileList.forEach(item => {
         formData.append("files", item.raw);
       });
+      // todo 回调刷新列表
       api.audioConvert(formData);
+    },
+    deleteFile(fileName) {
+      this.fileList.splice(this.fileList.indexOf(this.fileList.find(element => element.name === fileName)), 1);
+    },
+    formatAudioSize(row, column, value) {
+      return Math.floor(value / 1024 / 1024) + 'MB';
     }
   }
 }
